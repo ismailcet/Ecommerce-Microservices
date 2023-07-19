3b0ee5fe1b7f09531975d42abfa7cf232b945700
İsmail Çetin <ismailcet3@gmail.com>
2023-07-19 16:34:42 +0300

    Ecommerce Project With Microservices Starter Files

---
+++ ecommerce-product-service/src/test/java/com/ismailcet/ecommerceproductservice/EcommerceProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ismailcet.ecommerceproductservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
